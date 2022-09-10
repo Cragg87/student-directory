e0bba846f9c2b6be15851d0255a406fad33808b3
Miles Cragg <milescragg@gmail.com>
2022-09-10 16:41:40 +0100

    adds specifying files to save/load

--- directory.rb
+++ directory.rb
@@ -1,37 +1,11 @@
-@students = []
+@students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-end
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_students_list
-  if @students.count > 0
-    @students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  else puts "There are no students"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def interactive_menu
@@ -41,43 +15,66 @@ def interactive_menu
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    puts "Choose file"
+    file = STDIN.gets.chomp
+    save_students(file)
+    puts "Students have been saved to #{file}"
+  when "4"
+    puts "Choose file"
+    file = STDIN.gets.chomp
+    load_students(file)
+    puts "Students have been successfully uploaded from #{file}"
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
 end
 
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you want. Please try again."
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def save_students
-  file = File.open("students.csv", "w")
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students(filename = "students.csv")
+  file = File.open(filename, "w")
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+    student_data = [student[:name], student[:cohort]].join(",")
+    file.puts student_data
   end
   file.close
 end
@@ -85,23 +82,23 @@ end
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-      @students << {name: name, cohort: cohort.to_sym}
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
 
-def try_load_students
+def startup_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exist?(filename)
+  filename = "students.csv" if filename.nil?
+  if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
-  else
+  else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
-    exit
+    exit # quit the program
   end
 end
 
-try_load_students
+startup_load_students
 interactive_menu
